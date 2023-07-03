8759b7395a0fff72859b53016dc0ebf43879e45d
Glinglin <le.neko@gmail.com>
2023-07-04 01:32:00 +0200

    fix: missing crate top level documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,20 @@
-#![deny(missing_docs)]
-
+#![warn(missing_docs)]
+//! Processing yaml with include documents through `!include <path>` tag.
+//!
+//! ## Features
+//!
+//! - include and parse recursively `yaml` (and `json`) files
+//! - include `markdown` and `txt` text files
+//! - include other types as `base64` encoded binary data.
+//! - optionaly handle gracefully circular references with `!circular` tag
+//!
+//! ## Example
+//! ```
+//! let path = PathBuf::from("/path/to/yaml/file");
+//! let transformer = transformer::Transformer::new(path)?;
+//!
+//! println!("{}", transformer);
+//! ```
 mod helpers;
 mod transformer;
 
